7933d61ea0b0065b395887ed6f76b09d45dd1025
Eric Friesen <efriesen@gmail.com>
2013-07-18 15:50:52 -0700

    comments and some cleanups

--- lydia/src/ca/efriesen/lydia/fragments/LauncherFragment.java
+++ lydia/src/ca/efriesen/lydia/fragments/LauncherFragment.java
@@ -7,9 +7,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.*;
-import android.location.Address;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +29,6 @@ public class LauncherFragment extends Fragment implements
 
 	private Activity activity;
 	private PackageManager packageManager;
-	private List<PackageInfo> installedApps;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
@@ -47,10 +44,11 @@ public class LauncherFragment extends Fragment implements
 		activity = getActivity();
 		packageManager = activity.getPackageManager();
 
+		// find the listview and attach the appinfo adapter to it
 		ListView listView = (ListView) getActivity().findViewById(R.id.application_list);
 		listView.setAdapter(new AppInfoViewAdapter(getInstalledPackages(), getActivity()));
+		// send any item clicks back to this class, looking for method onItemClick
 		listView.setOnItemClickListener(this);
-
 	}
 
 	// if the back button is pressed while we're visible, go back to home screen one
@@ -61,33 +59,45 @@ public class LauncherFragment extends Fragment implements
 				.show(manager.findFragmentById(R.id.home_screen_fragment))
 				.addToBackStack(null)
 				.commit();
+		// we've consumed the back press
 		return true;
 	}
 
-	private ArrayList getInstalledPackages() {
+	// returns an array of appinfos of the installed packages we can launch
+	private ArrayList<AppInfo> getInstalledPackages() {
+		// create our new arrays
 		ArrayList<AppInfo> appInfos = new ArrayList<AppInfo>();
-		List<ResolveInfo> activityList = getActivityList();
+		// get the list of all installed apps
+		List<PackageInfo> installedApps = packageManager.getInstalledPackages(0);
+		// get a list of activites with the "Action Main" intent
+		List<ResolveInfo> activityList = packageManager.queryIntentActivities(new Intent(Intent.ACTION_MAIN, null), 0);
 
-		installedApps = packageManager.getInstalledPackages(0);
-
+		// loop over the installed apps, and get the package info
 		for (PackageInfo info : installedApps) {
+			// create a new appinfo object
 			AppInfo appInfo = new AppInfo();
-			appInfo.setAppName(info.applicationInfo.loadLabel(packageManager).toString());
+			// we set package name here because we test on it later
 			appInfo.setPackageName(info.packageName);
-			appInfo.setVersionName(info.versionName);
-			appInfo.setVersionCode(info.versionCode);
-			appInfo.setIcon(info.applicationInfo.loadIcon(packageManager));
 
+			// loop over all the activites with the "main intent"
 			for (ResolveInfo resolveInfo : activityList) {
+				// if the current packages matches one of the activies
 				if (info.packageName.equals(resolveInfo.activityInfo.applicationInfo.packageName)) {
+					// set the attributes needed
 					appInfo.setClassName(resolveInfo.activityInfo.name);
+					appInfo.setAppName(info.applicationInfo.loadLabel(packageManager).toString());
+					appInfo.setVersionName(info.versionName);
+					appInfo.setVersionCode(info.versionCode);
+					appInfo.setIcon(info.applicationInfo.loadIcon(packageManager));
 
+					// create a new intent to stuff into the appinfo object
 					Intent launchIntent = new Intent();
 					ComponentName component = new ComponentName(appInfo.getPackageName(), appInfo.getClassName());
 					launchIntent.setComponent(component);
 					launchIntent.setAction(Intent.ACTION_MAIN);
 					appInfo.setLaunchIntent(launchIntent);
 
+					// add the object to the array that will be returned
 					appInfos.add(appInfo);
 					break;
 				}
@@ -95,6 +105,7 @@ public class LauncherFragment extends Fragment implements
 
 		}
 
+		// sort the list by appname ignoring case
 		Collections.sort(appInfos, new Comparator<AppInfo>() {
 			@Override
 			public int compare(AppInfo appInfo, AppInfo appInfo2) {
@@ -102,29 +113,25 @@ public class LauncherFragment extends Fragment implements
 			}
 		});
 
+		// return the list
 		return appInfos;
 	}
 
-	private List<ResolveInfo> getActivityList() {
-		List<ResolveInfo> aList = packageManager.queryIntentActivities(new Intent(Intent.ACTION_MAIN, null), 0);
-
-		Collections.sort(aList, new ResolveInfo.DisplayNameComparator(packageManager));
-
-		return aList;
-	}
-
-
 	@Override
 	public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+		// get the info of what was clicked
 		AppInfo appInfo = (AppInfo) adapterView.getAdapter().getItem(position);
 
+		// try to launch it
 		try {
 			startActivity(appInfo.getLaunchIntent());
 		} catch (Exception e) {
-			Toast.makeText(activity, "Launcher error", Toast.LENGTH_SHORT).show();
+			// uh oh, it failed.  show a warning message
+			Toast.makeText(activity, getString(R.string.launcher_error), Toast.LENGTH_SHORT).show();
 		}
 	}
 
+	// listview adapter for appinfos
 	class AppInfoViewAdapter extends BaseAdapter implements ListAdapter {
 		private final List<AppInfo> content;
 		private final Activity activity;
@@ -147,19 +154,24 @@ public class LauncherFragment extends Fragment implements
 		}
 
 		public View getView(int position, View convertView,	ViewGroup parent) {
+			// inflate the view if not already done
 			if (convertView == null) {
 				LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 				convertView = layoutInflater.inflate(R.layout.app_launcher_row, null);
 			}
 
+			// get the specific app we've pressed
 			AppInfo appInfo = content.get(position);
 			if (appInfo != null) {
+				// get the name and icon views
 				TextView appName = (TextView) convertView.findViewById(R.id.app_launcher_row_app_name);
 				ImageView appIcon = (ImageView) convertView.findViewById(R.id.app_launcherrow_icon);
 
+				// set the name and icon
 				appName.setText(appInfo.getAppName());
 				appIcon.setImageDrawable(appInfo.getIcon());
 			}
+			// return the view
 			return convertView;
 		}
 	}
