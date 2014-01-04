1a5f6c7c44b23a6c6a67d140462c105d273c7bd4
Eric Friesen <efriesen@gmail.com>
2014-01-03 21:41:45 -0800

    increase build number
    increase build number
    
    Signed-off-by: Eric Friesen <efriesen@gmail.com>

--- lydia/src/ca/efriesen/lydia/activities/Dashboard.java
+++ lydia/src/ca/efriesen/lydia/activities/Dashboard.java
@@ -63,7 +63,7 @@ public class Dashboard extends Activity {
 
 		// don't include bug sense it the key hasn't been changed
 		if (!getString(R.string.bugsenseApiKey).equalsIgnoreCase("Your Bugsense Key")) {
-			String build = "0.01-1";
+			String build = "0.01-4";
 			BugSenseHandler.addCrashExtraData("build", build);
 //			BugSenseHandler.initAndStartSession(Dashboard.this, getString(R.string.bugsenseApiKey));
 		}
